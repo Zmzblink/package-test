2638c8e099be6d06cdbaef7046f70a9ff41e7cc7
Zmzblink <zmzblink@qq.com>
2019-07-18 18:33:21 +0800

    init package

---
+++ zmzblink/package-test/src/Facade.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zmzblink\PackageTest;
+use Illuminate\Support\Facades\Facade as LaravelFacade;
+
+class Facade extends LaravelFacade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'test';
+    }
+}
+
